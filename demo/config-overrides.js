c6c632b24049076c9c6ffa8af0723fa6aab21267
Marius Andra <marius.andra@gmail.com>
2021-07-06 10:59:25 +0200

    simplify config

--- demo/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { aliasJest } = require('react-app-rewire-alias')
-const { aliasDangerous } = require('react-app-rewire-alias/lib/aliasDangerous')
-
-const aliasMap = {
-  'kea-forms': '../src',
-}
-
-module.exports = aliasDangerous(aliasMap)
-module.exports.jest = aliasJest(aliasMap)
